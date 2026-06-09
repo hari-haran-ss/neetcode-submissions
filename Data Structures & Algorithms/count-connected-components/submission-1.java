df72b019eeddd5a351a3ce1f948932b1f050f35f
Hariharan SenthilVel <hariharan.senthilvel@apollo.io>
2026-06-09 20:58:31 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            map.put(i, new ArrayList<>());
+        }
+        for(int[] edge: edges){
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+        Set<Integer> visited = new HashSet<>();
+        int result = 0;
+        for(int i=0; i<n; i++){
+            if(visited.contains(i))
+                continue;
+            dfs(i, map, visited, -1);
+            result++;
+        }
+        return result;
+    }
+
+    public void dfs(int node, Map<Integer, List<Integer>> map, Set<Integer> visited, int parent){
+        visited.add(node);
+        for(int ele: map.get(node)){
+            if(!visited.contains(ele))
+                dfs(ele, map, visited, node);
+        }
+    }
+}
